fd7438b640581870a30e3e39d571bb15e410ad6e
Iona <ionabrabender@gmail.com>
2020-05-07 18:27:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -188,6 +188,7 @@ end
 
 def big_shoe_rebounds
   largest_shoe_size = 0
+  player_w_largest_shoe.new 
   game_hash.each do |key, value|
     value[:players].each do |attribute, data|
       if attribute[:shoe] > largest_shoe_size
